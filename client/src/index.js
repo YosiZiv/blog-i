0ac42aeeffd3e058c12869b1c665b9a886777ae5
katlany <notan8@gmail.com>
2019-09-16 02:43:52 +0300

    redux import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
